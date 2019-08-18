4e397fe12052e27e31cafc0616a7a40481d7a6b8
Timur Manyanov <gtibalt@gmail.com>
2019-08-19 03:17:04 +0700

    Add collision with sprite removal

--- helpers.js
+++ helpers.js
@@ -5,3 +5,29 @@ export function degreesToRadians(degrees) {
 export function range(n) {
     return [...Array(n).keys()];
 }
+
+export function isColliding(sprite1, sprite2) {
+    const dx = sprite1.x - sprite2.x;
+    const dy = sprite1.y - sprite2.y;
+    const width1 = sprite1.width || sprite1.radius * 2;
+    const width2 = sprite2.width || sprite2.radius * 2;
+
+    return Math.sqrt(dx * dx + dy * dy) < width1 / 2 + width2;
+}
+
+export function findColliding(sprite1, sprites2) {
+    return sprites2.find(sprite2 => isColliding(sprite1, sprite2));
+}
+
+export function findAllColliding(sprites1, sprites2) {
+    return sprites1.reduce((allColliding, sprite1) => {
+        const colliding = findColliding(sprite1, sprites2);
+
+        if (colliding) {
+            allColliding.push(sprite1);
+            allColliding.push(colliding);
+        }
+
+        return allColliding;
+    }, []);
+}
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import { init,
-    initPointer, pointer, onPointerDown, keyPressed,
-    Sprite, GameLoop
-} from './node_modules/kontra/kontra.mjs'
+import {
+    GameLoop,
+    init,
+    initPointer,
+    onPointerDown,
+    pointer
+} from './node_modules/kontra/kontra.mjs';
 import Player from './player.js';
 import Enemy from './enemy.js';
 import Bullet from './bullet.js';
-import { range } from './helpers.js';
+import { range, findAllColliding } from './helpers.js';
 
 console.log('Initializing game engine...');
 
@@ -13,38 +16,33 @@ const { canvas } = init();
 initPointer();
 
 let sprites = [];
-let lastFired = 0;
 
-const player1 = Player({ id: 'player-1', canvas, pointer });
-sprites.push(player1);
+createSprites();
 
 onPointerDown((e, object) => {
     console.log('FIRE');
-    lastFired += 1/60;
-    if (lastFired > 1/60) {
-        lastFired = 0;
-        console.log('BULLET')
-        sprites.push(Bullet(player1));
-    }
-});
+    const player = sprites.find(s => s.type === 'player');
 
-const enemies = range(5).map(n => {
-    const id = `enemy-${n}`;
-    console.log(`Creating new enemy ${id}...`);
-    const enemy = Enemy(id);
-    sprites.push(enemy);
-
-    return enemy;
+    sprites.push(Bullet(player));
 });
 
 GameLoop({
     update() {
-        sprites
-            .map(s => {
-                s.update();
-                return s;
-            })
-            .filter(s => s.isAlive());
+        const updatedSprites = sprites.map(s => {
+            s.update();
+            return s;
+        });
+
+        const bullets = updatedSprites.filter(s => s.type === 'bullet');
+        const enemies = updatedSprites.filter(s => s.type === 'enemy');
+
+        findAllColliding(bullets, enemies).forEach(s => {
+            s.ttl = 0;
+        });
+
+        sprites = updatedSprites.filter(s => s.isAlive());
+
+        console.log('SPRITES', sprites.length);
     },
     render() {
         sprites.forEach(s => {
@@ -52,3 +50,16 @@ GameLoop({
         });
     }
 }).start();
+
+function createSprites() {
+    const player1 = Player({ id: 'player-1', canvas, pointer });
+    sprites.push(player1);
+
+    const enemies = range(5).map(n => {
+        const id = `enemy-${n}`;
+        console.log(`Creating new enemy ${id}...`);
+        return Enemy(id);
+    });
+
+    sprites.push(...enemies);
+}
